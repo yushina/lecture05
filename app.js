bb03b6d8a08a9bb46af7614067f31dd102a618da
yushina <s.g.wakita.model@gmail.com>
2015-05-25 02:12:30 +0900

    練習問題３まで説きました

--- app.js
+++ app.js
@@ -52,13 +52,6 @@ var player = document.querySelector("audio"); player.playbackRate = value; showP
 }
 
 
-
-
-
-
-
-
-
 var skip15secButton = document.querySelector("#skip");
 skip15secButton.addEventListener("click",  onSkip15secButtonClicked);
 
@@ -71,5 +64,39 @@ rewindToTopButton.addEventListener("click",  onRewindToTopButtonClicked);
 var enableLoopButton = document.querySelector("#enableLoop");
 enableLoopButton.addEventListener("click",  onEnableLoopButtonClicked);
 
+/*
 var backx1Button = document.querySelector("#backx1");
 backx1Button.addEventListener("click",  onbackx1ButtonClicked);
+*/
+
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+// 描画コンテキスト
+
+
+function draw(){
+ctx.fillStyle = "rgb(0, 0, 0)";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "rgb(192, 192, 192)";
+ctx.fillRect(canvas.width / 2 - 5, canvas.height / 2 - 5,10, 10);
+}
+
+
+
+function draw(){
+  ctx.fillStyle = "rgb(32, 32, 32)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  var data = [20, 40, 50, 60, 30, 10, 20, 40, 70, 5, -1, -20, -5, -20, -7, -5, 5];
+  ctx.fillStyle = "rgb(255, 255, 255)";
+
+  var i = 0;
+  var offset = 5;
+  var width = (canvas.width - offset * (data.length + 1))/ data.length;
+  while(i < data.length){
+    var x = offset * (i + 1) + width * i;
+      ctx.fillRect(x, canvas.height / 2 - data[i] * 4, 
+                   width, data[i] * 4);
+      i = i + 1;
+  }
+}
